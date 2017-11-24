1e79990b7dd7fb0d3aca978ee8583d625fa74bad
Tomas <tomas.barcak@gmail.com>
2017-11-24 15:56:48 +0000

    adding click to restart

--- mapka.js
+++ mapka.js
@@ -1,21 +1,35 @@
-let veta = '#####################                  ##   x    ###       ##  #          #    ##                  ##       #          ##                  #####################';
-let mapka = veta.split("")
+let veta =
+	'##################################################                                      #        ##                  ë                   #        ##             ###############          #        ##                                               ##     #                                         ##                            #                  ##             #              #                  ##                            ######             ##                                               ##      #######                            ########                     #       #                 ##                   o                           ##                                               ##################################################';
+let mapka = veta.split('');
 let mapDiv = document.getElementById('map');
 let posOld;
 
-let filler = () => {
+let walker = () => {
+	let rX = Math.round(Math.random() * 2) - 1;
+	let rY = Math.round(Math.random() * 2) - 1;
+	let pos = veta.indexOf('ë') + rX + 49 * rY;
 
-    let rX = Math.round(Math.random() * 2) - 1;
-    let rY = Math.round(Math.random() * 2) - 1;
-    let pos = veta.indexOf('x') + rX + 20 * rY;
+	if (mapka[pos] == 'o') {
+		veta = mapka.join('');
+		clearInterval(intervale);
+		return (mapDiv.innerHTML = `<PRE>${veta.match(/.{49}/g).join('</br>')}</br>Zradloooooo!!!!</PRE>`);
+	}
 
-    if (mapka[pos] != '#' || posOld == pos) {
-        mapka[veta.indexOf('x')] = ' ';
-        mapka[pos] = 'x';
-        posOld = pos;
-    }
+	if (mapka[pos] != '#' || posOld == pos) {
+		mapka[veta.indexOf('ë')] = ' ';
+		mapka[pos] = 'ë';
+		posOld = pos;
+	}
 
-    veta = mapka.join("")
-    return mapDiv.innerHTML = `<PRE>${veta.match(/.{20}/g).join("</br>")}</PRE>`;
+    veta = mapka.join('');
+   
+    return (mapDiv.innerHTML = `<PRE>${veta.match(/.{49}/g).join('</br>')}</PRE>`);
+    
+};
 
+function interval() {
+    intervale = setInterval(walker, 100);
 }
+
+let intervale = setInterval(walker, 100);
+
